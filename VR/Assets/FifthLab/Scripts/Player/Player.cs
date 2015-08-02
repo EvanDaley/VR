d32747b480dbdcf2b273694af72f78037c282e50
Evan Daley <daley.evan@gmail.com>
2015-08-02 04:37:30 -0700

    Beast

--- VR/Assets/FifthLab/Scripts/Player/Player.cs
+++ VR/Assets/FifthLab/Scripts/Player/Player.cs
@@ -7,15 +7,20 @@ public class Player : MonoBehaviour {
 	public GameObject spaceCursor;
 	public Transform spaceCursorTransform;
 
+	public Vector3 moveTarget;
+
 	public GameObject selected;
 
 	private Transform _transform;
 	private CardboardHead _cardHead;
+	private NavMeshAgent _navMeshAgent;
 
 	// Use this for initialization
 	void Start () {
 		_transform = GetComponent<Transform>();
-
+		_cardHead = GetComponentInChildren<CardboardHead>();
+		_navMeshAgent = GetComponent<NavMeshAgent>();
+		_navMeshAgent.updateRotation = false;
 
 		if(spaceCursor == null)
 		{
@@ -33,11 +38,20 @@ public class Player : MonoBehaviour {
 
 			// switch(decide what to do based on selection and gaze target)
 
-			selected = GameObject.CreatePrimitive (PrimitiveType.Cube) as GameObject;
-			selected.transform.position = spaceCursorTransform.position;
+			//selected = GameObject.CreatePrimitive (PrimitiveType.Cube) as GameObject;
+			//selected.transform.position = spaceCursorTransform.position;
 			// create object and give it the "holo" script so it can float, rotate, and slide without crashing into things
 		
-		
+			Ray ray = _cardHead.Gaze;
+			RaycastHit hit;
+			if(Physics.Raycast (ray,out hit,50))
+			{
+				if(hit.collider.tag == "Ground")
+				{
+					moveTarget = hit.point;
+					_navMeshAgent.SetDestination (moveTarget);
+				}
+			}
 
 		}
 	}
