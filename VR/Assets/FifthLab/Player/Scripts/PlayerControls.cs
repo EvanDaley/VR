6205537622f3337aa024972137fc33445b972e14
Evan <daley.evan@gmail.com>
2015-08-10 21:34:02 -0700

    Make UI_Foreground Layer Stay in Foreground (second cam)

---
+++ VR/Assets/FifthLab/Player/Scripts/PlayerControls.cs
@@ -0,0 +1,29 @@
+/* Author: Evan Daley 
+ * Date: 8/10/2015 8:52:32 PM
+ * Revision: 0
+ * Modified Date: 
+ * Description: The basic mechanics that let the user control the player. 
+ * Watches for the click event and determines what to do based on what
+ * the player is looking at.
+ */ 
+  
+using UnityEngine;
+using System.Collections;
+  
+public class PlayerControls : MonoBehaviour {
+
+	public void Start()
+	{
+	
+	}
+
+	public void Update()
+	{
+	
+	}
+
+	public void OnTriggerEnter()
+	{
+	
+	}
+}
