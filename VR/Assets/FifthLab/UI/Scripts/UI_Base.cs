96f185fcc260ab98f5fe86ed5df938b2a54ea91c
Evan <daley.evan@gmail.com>
2015-08-10 19:47:33 -0700

    UI and Terrain

---
+++ VR/Assets/FifthLab/UI/Scripts/UI_Base.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UI_Base : MonoBehaviour {
+
+	protected Canvas _Canvas;
+
+	void Start()
+	{
+		_Canvas = GetComponentInParent<Canvas>();
+	}
+}
