ea0b626ce7bf80a4d775b8ca4111888f26976cc1
Evan <daley.evan@gmail.com>
2015-09-21 15:24:36 -0700

    Setting Up Networking

---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/NetworkBackdoor.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkBackdoor : MonoBehaviour {
+
+    private NetworkManager m_NetworkManager;
+
+	// Use this for initialization
+	void Start () {
+        m_NetworkManager = GetComponent<NetworkManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown(KeyCode.C))
+        {
+            if(!m_NetworkManager.isNetworkActive)
+                m_NetworkManager.StartClient();
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            if (!m_NetworkManager.isNetworkActive)
+                m_NetworkManager.StartHost();
+        }
+    }
+}
