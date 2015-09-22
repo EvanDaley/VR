9605607089917c54ad2104ce60c92fc7eea4c9ed
Evan <daley.evan@gmail.com>
2015-09-21 17:21:52 -0700

    Improve Character

---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/General/LocalSettings.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LocalSettings : MonoBehaviour {
+
+    [HideInInspector]
+    public static LocalSettings instance;
+
+    public bool autoDetectRift = true;
+
+    void Awake()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/VR/PlayerSwitcher.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.VR;
+
+public class PlayerSwitcher : MonoBehaviour {
+
+    public GameObject normalPlayer;
+    public GameObject riftPlayer;
+
+    void Update () {
+	    if(LocalSettings.instance.autoDetectRift)
+        {
+            if (VRDevice.isPresent) {
+                UseRift();
+            } else {
+                UseNormal();
+            }
+        }
+    }
+
+    public void UseRift()
+    {
+        // Disable Normal Camera
+        normalPlayer.SetActive(false);
+
+        // Enable Rift
+        riftPlayer.SetActive(true);
+    }
+
+    public void UseNormal()
+    {
+        // Enable Normal Camera
+        normalPlayer.SetActive(true);
+
+        // Disable Rift
+        riftPlayer.SetActive(false);
+    }
+}
