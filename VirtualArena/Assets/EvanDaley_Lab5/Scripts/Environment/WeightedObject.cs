504f779647ea3f4c7c3a56111630422abeed643e
Evan <daley.evan@gmail.com>
2015-09-28 22:46:38 -0700

    Add Targeting

---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/Environment/WeightedObject.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WeightedObject : MonoBehaviour {
+
+    float mass = 35;
+
+    Rigidbody m_rbody;
+
+    void Start()
+    {
+        m_rbody = GetComponent<Rigidbody>();
+
+        if(m_rbody)
+        {
+            mass = m_rbody.mass;
+        }
+    }
+}
---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/Player/Targeting.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Targeting : MonoBehaviour {
+
+    // The current target we are looking at
+    private Transform targetTransform;
+    private Ray ray;
+    private RaycastHit hit;
+
+	void Start ()
+    {
+
+	}
+	
+	void Update () {
+        ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward * 1000);
+
+        if(Physics.Raycast(ray, out hit, 100F))
+        {
+            targetTransform = hit.transform;
+        }
+	}
+
+    /// <summary>
+    /// Returns the current transform that the player is looking at.
+    /// </summary>
+    public Transform Target
+    {
+        get
+        {
+            if (targetTransform != null)
+                return targetTransform;
+            else
+                return null;
+        }
+    }
+}
