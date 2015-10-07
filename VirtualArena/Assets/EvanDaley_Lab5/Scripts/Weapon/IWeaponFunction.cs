d6bce764320c531f041b8d40697302524cfab822
Evan <daley.evan@gmail.com>
2015-10-06 20:23:50 -0700

    Organize project

---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/Weapon/IWeaponFunction.cs
@@ -0,0 +1,13 @@
+/* Author: Evan Daley 
+ * Date: 10/6/2015 3:21:16 PM
+ * Revision: 0
+ * Modified Date: 
+ * Description: An interface that all weapons functions should inherit from. Requires the weapon to have a "Fire" method. 
+ */ 
+  
+using UnityEngine;
+using System.Collections;
+  
+public class IWeaponFunction : MonoBehaviour {
+
+}
