f77c09ea3c09449930d5b596640de020feac609d
Evan <daley.evan@gmail.com>
2015-10-26 23:22:28 -0700

    Animate Right Arm
    Create a Mecanim Animator and masks for the arms. Left arm will hold a
    weapon and right arm will cast spells.

---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/Networked/Actor/Cast/RightHand.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RightHand : MonoBehaviour {
+
+	/// <summary>
+	/// The animation parameter that will determine which animation to play. 
+	/// </summary>
+	public string animParm = "Throw";
+
+	/// <summary>
+	/// The animator component.
+	/// </summary>
+	private Animator m_Anim;
+
+	void Start () {
+		m_Anim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//m_Anim.SetBool(animParm, false);
+
+		if(Input.GetButtonDown("Fire2"))
+		{
+			//m_Anim.SetBool(animParm, true);
+			m_Anim.Play(animParm);
+		}
+	}
+}
