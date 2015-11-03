fa2f438177e7529d1e36bdc829055c6039a0bdf5
Evan <daley.evan@gmail.com>
2015-11-02 23:21:30 -0800

    Add Movement Reference Scripts

---
+++ Reference Scripts/CharacterPositionSync.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// Only use this on characters that NEED to be in the PERFECTLY synchronized positions. 
+/// ThirdPersonCharacterSinc usually works with .1 to .4 meters accuracy and causes less
+/// network traffic.
+/// </summary>
+public class CharacterPositionSync : MonoBehaviour {
+
+	PhotonView photonView;
+	Vector3 actualPosition;
+
+	// Use this for initialization
+	void Start () {
+		photonView = GetComponent<PhotonView> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (photonView.isMine)
+			photonView.RPC ("BroadcastPosition", PhotonTargets.Others, transform.position);
+
+		else {
+			float distance = Vector3.Distance (actualPosition, transform.position);
+			if (distance > .15f) {
+				//we want to lerp quickly if the character is way out of sync and 
+				//slowly if they are relatively close to where they shoul be
+				float jumpDistance = distance/10;
+				Vector3 next = Vector3.Lerp (transform.position,actualPosition,jumpDistance);
+				transform.position = next;
+				//print (gameObject.name + " nudged (" + next + ")");
+			}
+		}
+	}
+
+	[RPC]
+	void BroadcastPosition(Vector3 actualPosition)
+	{
+		this.actualPosition = actualPosition;
+	}
+}
---
+++ VirtualArena/Assets/EvanDaley_Lab5/Scripts/InstaSpawn/PlayerSpawner.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Cameras;
+
+public class PlayerSpawner : MonoBehaviour {
+
+	public bool autoConnect = true;
+	public string playerPrefab;
+	public GameObject cameraPrefab;
+
+	private PhotonView m_PhotonView;
+
+	void Start () {
+		m_PhotonView = GetComponent<PhotonView>();
+
+		if(autoConnect)
+		{
+			PhotonNetwork.ConnectUsingSettings("0.1");
+		}
+	}
+
+	void OnConnectedToMaster()
+	{
+		JoinOrCreateRoom();
+	}
+
+	public void JoinOrCreateRoom()
+	{
+		PhotonNetwork.JoinOrCreateRoom("Room1", new RoomOptions(){}, TypedLobby.Default);
+	}
+
+	public void OnJoinedRoom()
+	{
+		SpawnNewPlayer ();
+	}
+
+
+	// TODO: PASS IN A LOCATION OR FIND ONE
+	void SpawnNewPlayer()
+	{
+		// get the main local camera so we can deactivate it
+		GameObject cam = Camera.main.gameObject;
+
+		// create the player
+		GameObject player = PhotonNetwork.Instantiate(playerPrefab,transform.position,transform.rotation,0);
+
+		// create the camera and make it target the new player
+		GameObject newCam = GameObject.Instantiate (cameraPrefab,transform.position,transform.rotation) as GameObject;
+		FreeLookCam m_cam = newCam.GetComponent<FreeLookCam>();
+		m_cam.SetTarget(player.transform);
+
+		// deactivate original camera
+		cam.SetActive (false);
+	}
+
+	void RespawnPlayer()
+	{
+		// reset and resuse player
+	}
+
+	void Update () {
+		
+	}
+}
