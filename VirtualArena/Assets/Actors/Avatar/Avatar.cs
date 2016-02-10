2f2d238f15130715589eabecf95c66778503236c
Evan <daley.evan@gmail.com>
2016-02-09 20:42:58 -0800

    Add Avatar Class

---
+++ VirtualArena/Assets/Actors/Avatar/Avatar.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Avatar : MonoBehaviour, IKillable, IDamageable<float>
+{
+	// The health of this unit. Use some sort of complicated algorithm for generating health on spawn.
+	private float health = 100;	// TODO: Set based on monster's level.
+
+	//The required method of the IDamageable interface. This should use extremely complex algorithms 
+	// to account for shields, armor, vulnerabilities, area of hit, etc.
+	public void Damage(float damageTaken)
+	{
+		health -= damageTaken;
+
+		if (health < 0)
+		{
+			health = 0;
+			Kill ();
+		}
+	}
+
+	//The required method of the IKillable interface
+	public void Kill()
+	{
+		print ("Destroyed!");
+		GameObject.Destroy (gameObject);
+	}
+}
